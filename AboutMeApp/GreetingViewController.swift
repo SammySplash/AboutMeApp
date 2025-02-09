cdccec0afc2d44d656c220d14ecbe5edad4de6dc
GlebSamoilik <6321657@gmail.com>
2025-02-09 20:20:11 +0000

    Creating a GreetingViewController and setting up a background. Placing elements on the screens.

---
+++ AboutMeApp/GreetingViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GreetingViewController.swift
+//  AboutMeApp
+//
+//  Created by Samoilik Hleb on 09/02/2025.
+//
+
+import UIKit
+
+class GreetingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGradientBackground()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.layer.sublayers?.first?.frame = view.bounds
+    }
+    
+    func setGradientBackground() {
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = view.bounds
+        
+        gradientLayer.colors = [
+            UIColor.systemPink.withAlphaComponent(0.3).cgColor,
+            UIColor.systemIndigo.withAlphaComponent(0.3).cgColor
+        ]
+        
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
