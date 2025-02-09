ca12d1ee41094f544ec4305b67fc6b8a78f621d9
GlebSamoilik <6321657@gmail.com>
2025-02-09 13:18:19 +0000

    Preparing project

--- AboutMeApp/MainViewController.swift
+++ AboutMeApp/MainViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+private class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
 }
 
